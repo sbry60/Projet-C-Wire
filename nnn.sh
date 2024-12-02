fa7b8815841b594ec55adfebe3cdc177dc2f7d63
sbry60 <163690273+sbry60@users.noreply.github.com>
2024-12-02 15:31:03 +0100

    Update nnn.sh

--- nnn.sh
+++ nnn.sh
@@ -1,24 +1,12 @@
-#!/bin/bash
-
-
-chercher_motif_5_cwire() {
-    local fichier="cwire.dat" 
-    local motif="-"          
-
-    
-    if [[ ! -f "$fichier" ]]; then
-        echo "Erreur : Le fichier $fichier n'existe pas."
-        return 2
-    fi
-
-    while IFS= read -r ligne; do
-        if [[ ${#ligne} -ge 5 ]]; then
-            if [["${ligne:0:1}" == "1" && "${ligne:2:1}" != "$motif" ]]; then
-                echo "$ligne"
-            fi
-        fi
-    done < "$fichier"
-}
-
-
-chercher_motif_5_cwire
+#Les HVB
+awk -F';' '$2 != "-" && $3 == "-" && $4 == "-" && $5 == "-" && $6 == "-"' "cwire.dat"
+#les consommateurs des HVB
+awk -F';' '$2 != "-" && $4 == "-" && $7 == "-"' "cwire.dat"
+#les postes LV
+awk -F';' '$3 != "-" && $4 != "-" && $8 == "-"' "cwire.dat"
+#Les consommateurs des HVA
+awk -F';' '$3 != "-" && $5 != "-" && $7 == "-"' "cwire.dat"
+#Les consommateurs indiviuels des postes LV
+awk -F';' '$4 != "-" && $6 != "-" && $7 == "-"' "cwire.dat"
+#Les consommateurs entreprises des postes LV
+awk -F';' '$4 != "-" && $5 != "-" && $7 == "-"' "cwire.dat"
