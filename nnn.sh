6f02a01e75880e21f0137ce8c2d121f5437a0ee4
sbry60 <163690273+sbry60@users.noreply.github.com>
2024-12-02 18:37:19 +0100

    Update nnn.sh

--- nnn.sh
+++ nnn.sh
@@ -1,16 +1,17 @@
 #Les HVB
-awk -F';' '$2 != "-" && $3 == "-" && $4 == "-" && $5 == "-" && $6 == "-"' "cwire.dat"
-#les consommateurs des HVB
-awk -F';' '$2 != "-" && $4 == "-" && $7 == "-"' "cwire.dat"
+awk -F';' '$2 != "-" && $3 == "-" && $4 == "-" && $5 == "-" && $6 == "-"{print $2, $7}' "cwire.dat"
+#Les HVA
+awk -F';' '$2 != "-" && $3 != "-" && $4 == "-" ' "cwire.dat"
 #les postes LV
 awk -F';' '$3 != "-" && $4 != "-" && $8 == "-"' "cwire.dat"
+#les consommateurs des HVB
+awk -F';' '$2 != "-" && $4 == "-" && $7 == "-"' "cwire.dat"
 #Les consommateurs des HVA
 awk -F';' '$3 != "-" && $5 != "-" && $7 == "-"' "cwire.dat"
 #Les consommateurs indiviuels des postes LV
 awk -F';' '$4 != "-" && $6 != "-" && $7 == "-"' "cwire.dat"
 #Les consommateurs entreprises des postes LV
 awk -F';' '$4 != "-" && $5 != "-" && $7 == "-"' "cwire.dat"
-
 
 
 
