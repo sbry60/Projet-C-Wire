0dd17c371efc4bb68dd2ad5326fa7ea64389b872
IS277 <129891244+IS277@users.noreply.github.com>
2024-12-02 17:03:51 +0100

    Update nnn.sh

--- nnn.sh
+++ nnn.sh
@@ -10,3 +10,21 @@ awk -F';' '$3 != "-" && $5 != "-" && $7 == "-"' "cwire.dat"
 awk -F';' '$4 != "-" && $6 != "-" && $7 == "-"' "cwire.dat"
 #Les consommateurs entreprises des postes LV
 awk -F';' '$4 != "-" && $5 != "-" && $7 == "-"' "cwire.dat"
+
+
+
+
+#!/bin/bash
+
+if [[ "$1" == "hvb" ]] && [[ "$2" == "comp" ]]; then 
+  echo "HVB COMP"
+
+  # Les HVB
+  awk -F';' '$2 != "-" && $3 == "-" && $4 == "-" && $5 == "-" && $6 == "-"' "cwire.dat" |  sed 's/;-//g; s/-;//g'
+
+
+  echo "comssommmateurs"
+  # Les consommateurs des HVB
+  awk -F';' '$2 != "-" && $4 == "-" && $7 == "-"' "cwire.dat"
+fi
+
