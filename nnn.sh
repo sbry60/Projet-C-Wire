f42ef84c2eaaee1262c487bd28219586234af2c5
sbry60 <163690273+sbry60@users.noreply.github.com>
2024-12-15 18:22:07 +0100

    Update nnn.sh

--- nnn.sh
+++ nnn.sh
@@ -11,13 +11,36 @@ function help_me {
     echo "  -h                        : Affiche cette aide."
     exit 0
 }
-gcc -o projet projet.c
+
+if [ "$#" -lt 3 ]; then
+    echo "Erreur : Nombre d'arguments insuffisant."
+    help_me
+fi
+
+
+if [[ "$*" == *"-h"* ]]; then
+    help_me
+fi
+
 FILE=$1
 TYPE_STATION=$2
 TYPE_CONS=$3
 ID_CENTRALE=${4:-"ALL"} 
 
+max=
 
+{
+    read -r header
+    while IFS=';' read -r col1 _; do
+        if [ -n "$col1" ]; then
+            if [ -z "$max" ]; then
+                max=$col1
+            elif [ "$col1" -gt "$max" ]; then
+                max=$col1
+            fi
+        fi
+    done
+} < "$FILE"
 
 START_TIME=$(date +%s)
 
@@ -29,6 +52,7 @@ if [ ! -f "$FILE" ]; then
     help_me
     exit 1
 fi
+gcc -o projet projet.c
 if [[ "$TYPE_STATION" != "hvb" && "$TYPE_STATION" != "hva" && "$TYPE_STATION" != "lv" ]]; then
     echo "Erreur : Type de station incorrecte ($TYPE_STATION)."
     help_me
@@ -39,6 +63,11 @@ if [[ "$TYPE_CONS" != "comp" && "$TYPE_CONS" != "indiv" && "$TYPE_CONS" != "all"
     help_me
 fi
 
+if [[ "$ID_CENTRALE" -gt "$max"  ]]; then
+    echo "Erreur : identifiant centrale incorrecte ($ID_CENTRALE)."
+    help_me
+fi
+
 
 
 
@@ -46,21 +75,27 @@ if [[ "$ID_CENTRALE" != "ALL" ]]; then
   
   if [[ "$TYPE_STATION" == "hvb" ]] && [[ "$TYPE_CONS" == "comp" ]]; then 
     awk -F';' -v id="$ID_CENTRALE" '$1==id && $2 != "-" && $4 == "-" && $7 == "-" ||$1==id && $2 != "-" && $3 == "-" && $4 == "-" && $5 == "-" && $6 == "-"' $FILE | cut -d';' -f2,5,7,8 | tr '-' '0'| ./projet
+    mv monfichier.csv hvb_comp_$ID_CENTRALE.csv
 
 
   elif [[ "$TYPE_STATION" == "hva" ]] && [[ "$TYPE_CONS" == "comp" ]]; then 
     awk -F';' -v id="$ID_CENTRALE" '$1==id && $3 != "-" && $5 != "-" && $7 == "-" ||$1==id && $2 != "-" && $3 != "-" && $4 == "-" ' $FILE | cut -d';' -f3,5,7,8 | tr '-' '0'| ./projet
+    mv monfichier.csv hva_comp_$ID_CENTRALE.csv
+    
 
 
   elif [[ "$TYPE_STATION" == "lv" ]] && [[ "$TYPE_CONS" == "comp" ]]; then 
-    awk -F';' -v id="$ID_CENTRALE" '$1==id && $4 != "-" && $5 != "-" && $7 == "-" ||$1==id && $3 != "-" && $4 != "-" && $8 == "-"' $FILE | cut -d';' -f4,5,7,8 | tr '-' '0'| ./projet
+    awk -F';' -v id="$ID_CENTRALE" '$1==id && $4 != "-" && $5 != "-" && $7 == "-" ||$1==id && $3 != "-" && $4 != "-" && $8 == "-"' $FILE | cut -d';' -f4,5,7,8 | tr '-' '0'| ./
+    mv monfichier.csv lv_comp_$ID_CENTRALE.csv
 
 
   elif [[ "$TYPE_STATION" == "lv" ]] && [[ "$TYPE_CONS" == "indiv" ]]; then 
     awk -F';' -v id="$ID_CENTRALE" '$1==id && $4 != "-" && $6 != "-" && $7 == "-"|| $1 == id && $3 != "-" && $4 != "-" && $8 == "-"' $FILE | cut -d';' -f4,6,7,8 | tr '-' '0'| ./projet
+    mv monfichier.csv lv_indiv_$ID_CENTRALE.csv
 
   elif [[ "$TYPE_STATION" == "lv" ]] && [[ "$TYPE_CONS" == "all" ]]; then 
   awk -F';' -v id="$ID_CENTRALE" '$1==id && $4 != "-" && $7 == "-"||$1==id && $3 != "-" && $4 != "-" && $8 == "-"' $FILE | cut -d';' -f4,6,7,8 | tr '-' '0'| ./projet
+  mv monfichier.csv lv_all_$ID_CENTRALE.csv
 
   else
   echo "Erreur"
@@ -69,21 +104,26 @@ if [[ "$ID_CENTRALE" != "ALL" ]]; then
 else
   if [[ "$TYPE_STATION" == "hvb" ]] && [[ "$TYPE_CONS" == "comp" ]]; then 
     awk -F';' '$2 != "-" && $4 == "-" && $7 == "-" || $2 != "-" && $3 == "-" && $4 == "-" && $5 == "-" && $6 == "-"' $FILE| cut -d';' -f2,5,7,8 | tr '-' '0'| ./projet
+    mv monfichier.csv hvb_comp.csv
 
 
   elif [[ "$TYPE_STATION" == "hva" ]] && [[ "$TYPE_CONS" == "comp" ]]; then 
     awk -F';' '$3 != "-" && $5 != "-" && $7 == "-" || $2 != "-" && $3 != "-" && $4 == "-" ' $FILE | cut -d';' -f3,5,7,8 | tr '-' '0'| ./projet
+    mv monfichier.csv hva_comp.csv
 
 
   elif [[ "$TYPE_STATION" == "lv" ]] && [[ "$TYPE_CONS" == "comp" ]]; then 
     awk -F';' '$4 != "-" && $5 != "-" && $7 == "-" ||$3 != "-" && $4 != "-" && $8 == "-"' $FILE| cut -d';' -f4,5,7,8 | tr '-' '0'| ./projet
+    mv monfichier.csv lv_comp.csv
 
 
   elif [[ "$TYPE_STATION" == "lv" ]] && [[ "$TYPE_CONS" == "indiv" ]]; then 
     awk -F';'  '$4 != "-" && $6 != "-" && $7 == "-"||$3 != "-" && $4 != "-" && $8 == "-"' $FILE | cut -d';' -f4,6,7,8 | tr '-' '0'| ./projet
+    mv monfichier.csv lv_indiv.csv
 
   elif [[ "$TYPE_STATION" == "lv" ]] && [[ "$TYPE_CONS" == "all" ]]; then 
   awk -F';' '$4 != "-" && $7 == "-"||$3 != "-" && $4 != "-" && $8 == "-"' $FILE | cut -d';' -f4,6,7,8 | tr '-' '0'| ./projet
+  mv monfichier.csv lv_all.csv
 
   else
   echo "Erreur"
